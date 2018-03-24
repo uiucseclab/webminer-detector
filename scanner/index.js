23dfc1c6f1955d8508e1c83fadef94a47e64cb06
C4Phone <qq2900726136@gmail.com>
2018-03-23 21:00:59 -0500

    now it's able to run profilers. Output format: https://chromedevtools.github.io/devtools-protocol/tot/Profiler#type-Profile

--- scanner/index.js
+++ scanner/index.js
@@ -1,4 +1,42 @@
+// vim: set expandtab ts=2 sw=2:
 'use strict';
 
-const chromeLauncher = require('chrome-launcher')
-const crp = require('chrome-remote-interface')
+const chromeLauncher = require('chrome-launcher');
+const crp = require('chrome-remote-interface');
+const fse = require('fs-extra');
+
+async function sleep(t) {
+  await new Promise(resolve => {
+    setTimeout(resolve, t);
+  });
+}
+
+async function entryFunction(profilingDuration) {
+  try {
+    var chrome = await chromeLauncher.launch({
+      chromeFlags: ['--headless']
+    });
+    var client = await crp({
+      host: 'localhost',
+      port: chrome.port
+    });
+    const {Page, Profiler} = client;
+    // setup handlers
+		await Profiler.enable();
+		await Page.enable();
+    // enable events then start!
+    await Page.navigate({url: 'https://www.cryptonoter.me/demo.php'});
+		await Profiler.start();
+    await Page.loadEventFired();
+		await sleep(profilingDuration);
+		const profilerOutput = await Profiler.stop();
+		await fse.writeJson('profilerOutput.json', profilerOutput);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    chrome && chrome.kill();
+    client && (await client.close());
+  }
+}
+
+entryFunction(3000);
