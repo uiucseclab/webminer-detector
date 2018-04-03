558c5a848820c5878c46190593541d4e0e1a6014
C4Phone <qq2900726136@gmail.com>
2018-04-03 17:46:45 -0500

    It turns out that Google deprecated Profiler and switched to use Tracing APIs  instead.
    This commit found a way to capture a tracing profile so you can load it in Chrome by visiting "about:tracing"

--- scanner/index.js
+++ scanner/index.js
@@ -1,9 +1,12 @@
 // vim: set expandtab ts=2 sw=2:
 'use strict';
 
+// Referenced code from: https://github.com/paulirish/automated-chrome-profiling
+
 const chromeLauncher = require('chrome-launcher');
 const crp = require('chrome-remote-interface');
 const fse = require('fs-extra');
+const process = require('process');
 
 async function sleep(t) {
   await new Promise(resolve => {
@@ -11,46 +14,44 @@ async function sleep(t) {
   });
 }
 
-async function entryFunction(profilingDuration) {
+var TRACE_CATEGORIES = [
+  "blink",
+  "cc",
+  "toplevel",
+  "v8",
+];
+
+async function entryFunction(url, profilingDuration) {
   try {
     var chrome = await chromeLauncher.launch({
-      chromeFlags: ['--headless']
+      chromeFlags: []//'--headless', '--disable-gpu']
     });
     var client = await crp({
       host: 'localhost',
       port: chrome.port
     });
-    const {Page, Profiler, Performance} = client;
-    // setup handlers
-		await Profiler.setSamplingInterval({"interval": 100});
-		await Profiler.enable();
-		await Performance.enable();
+    const {Page, Profiler, Tracing} = client;
 		await Page.enable();
-
-    // enable events then start!
-    // await Page.navigate({url: 'https://www.cryptonoter.me/demo.php'});
-    await Page.navigate({url: 'https://www.buzzfeed.com/zoetillman/a-lawyer-who-admitted-lying-to-the-special-counsels-office?utm_term=.mh4gdNa8kp#.slbbXwyprN'});
-    // await Page.navigate({url: 'https://krakenbenchmark.mozilla.org/kraken-1.1/driver.html'});
-    // await Page.navigate({url: 'https://www.google.com/'});
-		await Profiler.start();
-
-		let metrics = [];
-		const metricsInterval = setInterval(async () => {
-			let m = await client.send('Performance.getMetrics');
-			metrics.push({"metrics": m, "time": Date.now()});
-		}, 100);
-		await Profiler.startPreciseCoverage(true, true);
+    await Page.navigate({url: url});
     await Page.loadEventFired();
+    console.log('Page is loaded. Starting profiler...')
+    await Tracing.start({
+      "categories": TRACE_CATEGORIES.join(',')
+    });
+
+    // Handle tracing events
+    const tracingOutput = [];
+    Tracing.dataCollected(data => data.value.forEach(x =>
+      tracingOutput.push(x)
+    ))
+
 		await sleep(profilingDuration);
-
-
-		clearInterval(metricsInterval);
-		const preciseProfilerOutput = await Profiler.takePreciseCoverage();
-		await Profiler.stopPreciseCoverage();
-		const profilerOutput = await Profiler.stop();
-		await fse.writeJson('preciseProfilerOutput.json', preciseProfilerOutput);
-		await fse.writeJson('profilerOutput.json', profilerOutput);
-		await fse.writeJson('performanceOutput.json', metrics);
+    console.log("Terminating...")
+    await Tracing.end();
+    console.log("Terminating...")
+    await sleep(3000)
+		await fse.writeJson('tracingOutput.json', tracingOutput);
+    console.log("Terminating...")
   } catch (err) {
     console.error(err);
   } finally {
@@ -59,4 +60,11 @@ async function entryFunction(profilingDuration) {
   }
 }
 
-entryFunction(10000);
+// await Page.navigate({url: 'https://www.cryptonoter.me/demo.php'});
+// await Page.navigate({url: 'https://www.buzzfeed.com/zoetillman/a-lawyer-who-admitted-lying-to-the-special-counsels-office?utm_term=.mh4gdNa8kp#.slbbXwyprN'});
+// await Page.navigate({url: 'https://krakenbenchmark.mozilla.org/kraken-1.1/driver.html'});
+// await Page.navigate({url: 'https://www.google.com/'});
+const url = process.argv[2];
+const duration = process.argv[3];
+const output = process.argv[4];
+entryFunction(url, duration, output);
