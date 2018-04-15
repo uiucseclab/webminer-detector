f9c4401e4eb60c0adf87d9667ed0cf6643611f68
C4Phone <qq2900726136@gmail.com>
2018-04-15 12:12:46 -0500

    connect cacheServer.js to profiler/analyzer

--- cacheServer/cacheServer.js
+++ cacheServer/cacheServer.js
@@ -4,6 +4,11 @@ var redis = require('redis');
 var validUrl = require('valid-url');
 var url = require("url");
 
+// Dependencies & Configurations for Profiler/Scanner
+const {runChromeProfiler} = require('../scanner/profiler');
+const {analyze} = require('../scanner/analyzer');
+const ANALYSIS_DURATION = 3000; // 3 seconds
+
 //for redis
 var RDS_PORT = 6379;
 var RDS_HOST = '127.0.0.1';
@@ -21,14 +26,14 @@ rdsClient.on('error', function(err) {
 //for json tcp server
 var port = 8333;
 var server = net.createServer();
-server.listen(port, function() {  
+server.listen(port, function() {
     console.log('server listening on %j', server.address());
 });
 server.on('connection', handleConnection);
 
 function handleConnection(socket) {
     //decorate net.Socket
-    socket = new JsonSocket(socket); 
+    socket = new JsonSocket(socket);
     socket.on('message', function(message) {
         console.log('received: ' + message.url);
         //check url validation and do url cleaning here
@@ -40,21 +45,38 @@ function handleConnection(socket) {
 
         var urlCheck = url.parse(message.url);
         urlCheck = urlCheck.hostname;
-        
-        rdsClient.get(urlCheck, function(err, data) {
+
+        rdsClient.get(urlCheck, async function(err, data) {
             if(err) {
                 socket.sendEndMessage({result: 'fail'});
                 return;
             }
-            if(data === null) {
-                //call profiling func.
-                var malicious = '0';
-                rdsClient.set(urlCheck, malicious, redis.print);
-                socket.sendEndMessage({result: 'success', malicious: malicious});
-            } else {
+            if(data !== null) {
                 console.log('[cache info] ' + data);
                 socket.sendEndMessage({result: 'success', malicious: data});
+                return;
             }
+
+            //call profiling func.
+            let maliciousLevel = null;
+            try {
+              const profile = await runChromeProfiler(message.url, ANALYSIS_DURATION, [
+                '--headless'
+              ]);
+              if (profile)
+                maliciousLevel = analyze(...profile);
+            } catch (err) {
+              console.log(err);
+              maliciousLevel = null;
+            }
+
+            if (maliciousLevel === null) {
+              socket.sendEndMessage({result: 'fail'});
+              return;
+            }
+
+            rdsClient.set(urlCheck, maliciousLevel, redis.print);
+            socket.sendEndMessage({result: 'success', malicious: maliciousLevel});
         })
     });
 }
