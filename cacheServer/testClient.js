c22fff475a4cfd5efdd09b6f81cd1c669c9a0948
C4Phone <qq2900726136@gmail.com>
2018-04-15 12:15:54 -0500

    testClient.js now accepts url as cmdline arg

--- cacheServer/testClient.js
+++ cacheServer/testClient.js
@@ -1,13 +1,17 @@
-var net = require('net');
-var JsonSocket = require('json-socket');
+const net = require('net');
+const JsonSocket = require('json-socket');
+const process = require('process');
 
-var port = 8333;
-var host = '127.0.0.1';
-var socket = new JsonSocket(new net.Socket());
+const port = 8333;
+const host = '127.0.0.1';
+const socket = new JsonSocket(new net.Socket());
+
+const urlToTest = process.argv[2] || 'https://www.google.com';
+
 socket.connect(port, host);
 //Don't send until we're connected
 socket.on('connect', function() {
-    socket.sendMessage({url: 'https://www.google.com'});
+    socket.sendMessage({url: urlToTest});
     socket.on('message', function(message) {
         console.log(message.result + ': ' + message.malicious);
     });
