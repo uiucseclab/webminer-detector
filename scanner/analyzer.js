01247f7fe15dcbcc99a6321099409cbbb6fc48de
C4Phone <qq2900726136@gmail.com>
2018-04-29 13:04:54 -0500

    implement FinalAlgo.ipynb in analyzer.js

--- scanner/analyzer.js
+++ scanner/analyzer.js
@@ -6,20 +6,31 @@ const fse = require('fs-extra');
 const process = require('process');
 const {runChromeProfiler} = require('./profiler');
 
-const url = process.argv[2];
-const profilingDuration = process.argv[3] || 30000; // By default, 30s
-
 const SAFE_CPU_LIMIT1 = 5;
 const SAFE_CPU_LIMIT2 = 1;
 const SAFE_CPU_LIMIT3 = 0.8;
 const TRACING_NCPU_LIMIT = 0.5;
+const TIME_WINDOW_SIZE = 10 * 1000; // 10ms
+const TIMELINE_THRESHOLD = 0.01; // 1% of chrome's cpu usage
+
+const whitelist = [
+  /^http(s)?:\/\/[a-zA-Z0-9\-_]+\.googleapis.com\//i,
+  /^http(s)?:\/\/[a-zA-Z0-9\-_]+\.googlesyndication.com\//i,
+];
+function isUrlWhitelisted(url) {
+  url = new String(url);
+  for(let x of whitelist) {
+    if (url.match(x))
+      return true;
+  }
+  return false;
+}
 
 // Returns true/false (or null if error)
 // false: Maybe malicious, need more investigation.
 // true: Definitely safe
-function averageCpuUsage(usageOutput) {
-  const cpuUsages = usageOutput.filter(x=>'cpu' in x).map(x => x['cpu']);
-  return _.mean(cpuUsages);
+function getCpuUsages(usageOutput) {
+  return usageOutput.filter(x=>'cpu' in x).map(x => x['cpu']);
 }
 function analyzeCpuUsages(usageOutput) {
   const cpuUsages = usageOutput.filter(x=>'cpu' in x).map(x => x['cpu']);
@@ -38,7 +49,7 @@ function incrementCount(dict, key, amount) {
 
 const tracingOutputTimestampScale = 1e6;
 
-function readTracingOutput(tracingOutput) {
+function getFCallsByIds(tracingOutput) {
   let fcalls = tracingOutput.filter(x=>x['name'] == 'FunctionCall');
   let fcallsByIds = {};
   fcalls.forEach(x => {
@@ -51,10 +62,35 @@ function readTracingOutput(tracingOutput) {
   _.forIn(fcallsByIds, (val, key) => {
     val.length && val[0].ph == 'E' && val.shift();
   });
+  return fcallsByIds;
+}
 
-  let callByFrames = {};
-  let callByNames = {};
-  let callByURLs = {};
+function getFCallTimeRange(tracingOutput) {
+  let tslist = tracingOutput.map(x => x.ts).filter(x => x);
+  let min = _.min(tslist)
+  let max = _.max(tslist)
+  if (min === undefined)
+    return null;
+  if (max === undefined)
+    return null;
+  return [min, max];
+}
+
+function newArr(size, elemBuilder) {
+  return Array.apply(null, Array(size)).map(elemBuilder);
+}
+
+function readTracingOutput(tracingOutput) {
+  const fcallsByIds = getFCallsByIds(tracingOutput);
+  const timeRange = getFCallTimeRange(tracingOutput);
+  if (!timeRange)
+    return null;
+  const [startTime, endTime] = timeRange;
+  const nTimeWindow = 1 + Math.floor((endTime - startTime) / TIME_WINDOW_SIZE);
+  let callByFrames = newArr(nTimeWindow, ()=>({}));
+  let callByNames = newArr(nTimeWindow, ()=>({}));
+  let callByURLs = newArr(nTimeWindow, ()=>({}));
+  let allFrames = new Set(), allNames = new Set(), allURLs = new Set();
 
   for(let key in fcallsByIds) {
     let calls = fcallsByIds[key];
@@ -67,36 +103,53 @@ function readTracingOutput(tracingOutput) {
     for (let [bcall, ecall] of zipped) {
       let info1 = (bcall.args || {}).data || {};
       let info2 = (ecall.args || {}).data || {};
-      let info = Object.assign({}, info1, info2);
-      let duration = ecall.tts - bcall.tts;
+      const info = Object.assign({}, info1, info2);
+      const bts = bcall.ts, ets = ecall.ts;
       // sanity check
       if (info1.functionName != info2.functionName)
         if (info2.functionName)
           return null;
-      if (!duration && duration !== 0)
+      if (bts === undefined || bts === null ||
+          ets === undefined || ets === null)
         return null;
 
       // store results
-      duration = duration / tracingOutputTimestampScale;
-      incrementCount(callByFrames, info.frame, duration);
-      incrementCount(callByNames, info.functionName, duration);
-      incrementCount(callByURLs, info.url, duration);
+      allFrames.add(info.frame);
+      allNames.add(info.functionName);
+      allURLs.add(info.url);
+      let ts = bts;
+      while (ts < ets) {
+        let slice_id = Math.floor((ts - startTime) / TIME_WINDOW_SIZE)
+        let nextTs = startTime + (slice_id + 1) * TIME_WINDOW_SIZE
+        let duration = Math.min(ets, nextTs - 1) - ts
+
+        incrementCount(callByFrames[slice_id], info.frame, duration);
+        incrementCount(callByNames[slice_id], info.functionName, duration);
+        incrementCount(callByURLs[slice_id], info.url, duration);
+        ts = nextTs;
+      }
     }
   }
 
-  let tslist = tracingOutput.map(x => x.ts).filter(x => x);
-  let from = _.min(tslist);
-  let to = _.max(tslist);
-  let profilingDuration = (to - from) / tracingOutputTimestampScale;
+  let profilingDuration = (endTime - startTime) / tracingOutputTimestampScale;
 
-  return {callByFrames, callByNames, callByURLs, profilingDuration};
+  return [
+    [[callByFrames, allFrames], [callByNames, allNames], [callByURLs, allURLs]],
+    profilingDuration
+  ];
 }
 
-function printCounters(ctr, topN = 5) {
-  ctr = _.map(ctr, (x, k) => [x, k]);
-  ctr = _.sortBy(ctr, ([x, k]) => -x);
-  for (let i = 0; i < topN && i < ctr.length; i++)
-    console.log(ctr[i][0], ctr[i][1]);
+function getTimeline(counters, key, cpuUsages) {
+  let data = [];
+  let tlen = counters.length;
+  let cpuLen = cpuUsages.length;
+  let avg_cpu = _.mean(cpuUsages);
+  counters.forEach((c, tid) => {
+    //let curr_cpu = cpuUsages[Math.floor(cpuLen * tid / tlen)];
+    let curr_cpu = avg_cpu;
+    data.push((c[key] || 0) / TIME_WINDOW_SIZE / curr_cpu);
+  });
+  return data;
 }
 
 // Returns 1,2,3 (or null if error)
@@ -105,30 +158,51 @@ function printCounters(ctr, topN = 5) {
 // 3: Definitely safe
 function analyze(tracingOutput, usageOutput) {
   tracingOutput = readTracingOutput(tracingOutput);
+  let cpuUsages = getCpuUsages(usageOutput);
   if (tracingOutput === null) return null;
-  let {callByFrames, callByNames, callByURLs, profilingDuration} = tracingOutput;
-  let totalRuntime = _.sum(_.map(callByFrames, x=>x));
+  let [countersAndKeys, profilingDuration] = tracingOutput;
 
-  console.log('JS CPU usage: ', totalRuntime / profilingDuration * 100);
-  console.log('Process CPU usage: ', averageCpuUsage(usageOutput));
-
-  console.log('----- callByFrames -----');
-  printCounters(callByFrames);
-  console.log('----- callByNames -----');
-  printCounters(callByNames);
-  console.log('----- callByURLs -----');
-  printCounters(callByURLs);
   console.log('----------------------');
-  console.log('profilingDuration: ', profilingDuration);
+  console.log('--------START---------');
+  console.log('----------------------');
+  console.log('AVERAGE Process CPU usage: ', _.mean(cpuUsages));
+  console.log('Profiling duration: ', profilingDuration);
+  console.log('----------------------');
 
   // JS scripts running in non-window frames to gain multithread advantage
-  if (undefined in callByFrames)
-    if (callByFrames[undefined] > profilingDuration * TRACING_NCPU_LIMIT)
-      return 1;
+  let none_frame = getTimeline(countersAndKeys[0][0], undefined, cpuUsages);
+  none_frame = _.sum(none_frame.map(x=>x > 0.02)) / none_frame.length;
+  console.log('None frame score:', none_frame);
+  console.log('----------------------');
+  if (none_frame >= 0.2)
+    return 1;
+  if (none_frame >= 0.01)
+    return 2;
 
   // The JS scripts simply used too much CPU.
   // NOTE: GPU operations won't be counted in `totalRuntime`
-  if (totalRuntime > profilingDuration * TRACING_NCPU_LIMIT)
+  //        only if profiler was ran on users' local desktop
+  let [urlCounters, allURLs] = countersAndKeys[2];
+  let badUrlsToScores = {};
+  allURLs.forEach(key => {
+    if (isUrlWhitelisted(key))
+      return;
+    let timeline = getTimeline(urlCounters, key, cpuUsages);
+    let sum = 0;
+    let prev_high = false;
+    timeline.forEach(x => {
+      if (prev_high && x > TIMELINE_THRESHOLD)
+        sum += x;
+      prev_high = x > TIMELINE_THRESHOLD
+    });
+    let score = sum / timeline.length;
+    if (score > 0.05)
+      badUrlsToScores[key] = score;
+  });
+  console.log("Bad urls and their scores:")
+  console.log(badUrlsToScores)
+  console.log('----------------------');
+  if (_.keys(badUrlsToScores).length > 0)
     return 2;
 
   return 3;
@@ -137,6 +211,9 @@ function analyze(tracingOutput, usageOutput) {
 module.exports = {analyze};
 
 require.main === module && (async function () {
+  const url = process.argv[2];
+  const profilingDuration = process.argv[3] || 30000; // By default, 30s
+
   try {
     let profilerResult = await runChromeProfiler(url, profilingDuration, [
       '--headless'
--- scanner/data-sources.sh
+++ scanner/data-sources.sh
@@ -6,12 +6,17 @@ exit 1
 fi
 mkdir -p "$folder"
 ts=`date +%s`
-npm start 'http://www.linuxelite.com.br/' "$folder/1-mal-$ts" 5000
-npm start 'https://eenteslavoorsimon.be/' "$folder/2-mal-$ts" 5000
-npm start 'https://news.google.com/news/?ned=us&gl=US&hl=en' "$folder/3-good-$ts" 5000
-npm start https://www.youtube.com/watch?v=v_mOkUk5CDw "$folder/4-good-vid-$ts" 5000
-npm start 'http://abcnews.go.com/US/pastor-church-attended-25000-steps-amid-misconduct-allegations/story?id=54383250' "$folder/5-good-ad-$ts" 5000
-npm start 'http://bitcointicker.co/' "$folder/6-good-ui-$ts" 5000
-npm start 'https://stackoverflow.com/questions/3216013/get-the-last-item-in-an-array' "$folder/7-good-$ts" 5000
-npm start 'https://www.choosechicago.com/restaurants/' "$folder/8-good-$ts" 5000
-npm start 'http://189.26.255.57:8081/' "$folder/9-mal-$ts" 5000
+
+runDataSource() {
+	node analyzer.js $1 $3
+}
+
+runDataSource 'http://www.linuxelite.com.br/' "$folder/1-mal-$ts" 5000
+runDataSource 'https://eenteslavoorsimon.be/' "$folder/2-mal-$ts" 5000
+runDataSource 'https://news.google.com/news/?ned=us&gl=US&hl=en' "$folder/3-good-$ts" 5000
+runDataSource https://www.youtube.com/watch?v=v_mOkUk5CDw "$folder/4-good-vid-$ts" 5000
+runDataSource 'http://abcnews.go.com/US/pastor-church-attended-25000-steps-amid-misconduct-allegations/story?id=54383250' "$folder/5-good-ad-$ts" 5000
+runDataSource 'http://bitcointicker.co/' "$folder/6-good-ui-$ts" 5000
+runDataSource 'https://stackoverflow.com/questions/3216013/get-the-last-item-in-an-array' "$folder/7-good-$ts" 5000
+runDataSource 'https://www.choosechicago.com/restaurants/' "$folder/8-good-$ts" 5000
+runDataSource 'http://189.26.255.57:8081/' "$folder/9-mal-$ts" 5000
